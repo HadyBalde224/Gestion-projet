24ae3884e507734b52b8ea05ecf6881ccc681bc4
HadyBalde224 <mamadouhady35@gmail.com>
2025-12-14 22:03:17 +0100

    Cycle 1 - Test tourner(1) depuis NORD : GREEN

--- src/main/java/com/exemple/personnage/Personnage.java
+++ src/main/java/com/exemple/personnage/Personnage.java
@@ -1,4 +1,16 @@
 package com.exemple.personnage;
 
 public class Personnage {
+    private Orientation orientation;
+
+    public Personnage() {
+        this.orientation = Orientation.NORD;
+    }
+
+    public Orientation tourner(int fois) {
+        if (fois == 1) {
+            orientation = Orientation.EST;
+        }
+        return orientation;
+    }
 }
---
+++ src/test/java/com/exemple/personnage/PersonnageTest.java
@@ -0,0 +1,18 @@
+package com.exemple.personnage;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonnageTest {
+
+    @Test
+    void testTourner1DepuisNord() {
+        // Arrange
+        Personnage personnage = new Personnage();
+
+        // Act
+        Orientation resultat = personnage.tourner(1);
+
+        // Assert
+        assertEquals(Orientation.EST, resultat);
+    }
+}
